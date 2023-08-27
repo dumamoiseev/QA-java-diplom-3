8042bbbef599eccf1e3bb162d04654a2e8868031
duma_electronik <duma_electronik@TeamCity>
2023-08-27 21:24:03 +0000

    TeamCity change in 'TESTTEST' project: Connection updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -36,8 +36,8 @@ project {
         githubAppConnection {
             id = "PROJECT_EXT_58"
             displayName = "TEST GitHub App"
-            appId = "777123999"
-            clientId = "777123"
+            appId = "777123999123"
+            clientId = "777123123"
             clientSecret = "credentialsJSON:5b131916-407d-4299-828f-6754ffa2e005"
             privateKey = "credentialsJSON:6b372973-33bc-4d52-a624-106c80ae2a17"
             ownerUrl = "https://github.com/username"
